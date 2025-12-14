25bd4da1d5f4ebf3dc7490d6a792a223579d4605
savvymonkeyfoo <mike@savvymonkey.foo>
2025-12-14 11:22:09 +1100

    Remove broken legacy analysis page

--- web/src/app/analysis/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-// This is a legacy route - redirect to home page
-// The actual analysis page is at /workshop/[id]/analysis
-export default function LegacyAnalysisPage() {
-    redirect('/');
-}
